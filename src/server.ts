29ae1cc9776dbeb9fe8b7845b28b055350a60ecc
LogicVirtuoso <happycoder1215@gmail.com>
2025-03-07 06:23:11 -0800

    feat: added readme and .env.example

--- src/server.ts
+++ src/server.ts
@@ -57,7 +57,7 @@ app.post(
   }
 );
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 if (process.env.NODE_ENV !== "test") {
   app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 }
